458ea7b7334a624de334e8af36d7066e225c9f2e
Amo <emperoreddy@gmail.com>
2022-10-19 12:59:45 +0300

    implemented validate_time

--- alarma.py
+++ alarma.py
@@ -1,15 +1,19 @@
 from datetime import datetime
-import os
+import os, sys
 from playsound import playsound
 import time
 
 
-# ora_alarma = "12:00 AM"
+# ora_alarma = "12:00 AM" 
+# 24 hour format
 def validate_time(ora_alarma):
-    if int(ora_alarma[:2]) > 12:
-        return "Invalid HOUR format! Please try again..."
+    if int(ora_alarma[:2]) > 23:
+        print ("Invalid HOUR format! Please try again...")
+        return True
     elif int(ora_alarma[3:5]) > 59:
-        return "Invalid MINUTE format! Please try again..."
+        print ("Invalid MINUTE format! Please try again...")
+        return True
+    else: return False
 
 def get_alarm_time(ora_alarma):
     ora = ora_alarma[:2]
@@ -24,8 +28,8 @@ def get_current_time():
     return ora, minut
 
 def alarm_sound_path():
-    # get alarm sound
-    alarm_sound = os.path.join(os.getcwd(), "Alarm Clock_alarm.wav")
+    # get alarm sound path
+    alarm_sound = os.path.join(os.getcwd(), "Alarm_Clock.wav")
     return alarm_sound
 
 
@@ -35,16 +39,21 @@ print(get_current_time())
 # -- main --
 try:
     ora_alarma = input("Introduceti ora la care vrei sa te trezesti astfel: HH:MM\n" )
-    validate_time(ora_alarma)
+    while validate_time(ora_alarma):
+        ora_alarma = input("Introduceti ora la care vrei sa te trezesti astfel: HH:MM\n" )
+              
 except ValueError as e:
     print(f"Invalid time format! Please try again... \n {e}")
-else:    
+    
+else:
+    # get ora and minut from given alarm time input    
     ora = get_alarm_time(ora_alarma)[0]
     minut = get_alarm_time(ora_alarma)[1]
 
     while True:
         ora_curenta, minut_curent = get_current_time()
-        print(ora_curenta, minut_curent)
+        # to see the current time, uncomment
+        #print(ora_curenta, minut_curent) 
         time.sleep(1)
         if ora == ora_curenta and minut == minut_curent:
             print("Wake Up!")
